52a3b1747f44de1923ca1cb068bcf1c8149eba3a
Izzat888 <izzatjorayev907@gmail.com>
2026-03-16 20:12:43 +0500

    header qismi boshlandi

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-           <h1>Hello world</h1>
+           <header className='header'>
+            <div className="container">
+              <div className="header__container">
+                <h1 className='logo'>LOGO</h1>
+                <ul className='header__list'>
+                  <li className='header__list'><a href="">Home</a></li>
+                  <li className='header__list'><a href="">About</a></li>
+                  <li className='header__list'><a href="">Contact</a></li>
+                </ul>
+              </div>
+            </div>
+           </header>
     </div>
   )
 }
