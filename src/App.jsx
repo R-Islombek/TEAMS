d0f0fd05663733b058c38a008a8b427c65e096d1
R-Islombek <islombekraximjanov97dev@gmail.com>
2026-08-31 16:28:32 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
- import './App.css'
-import Header from './components/Header/Header'
-import Hero from './components/Hero/Hero'
+import React from 'react'
 
-function App() {
-
+const App = () => {
   return (
-    <>
-       <Header/>
-       <Hero/>
-    </>
+    <div>
+          App
+    </div>
   )
 }
 
